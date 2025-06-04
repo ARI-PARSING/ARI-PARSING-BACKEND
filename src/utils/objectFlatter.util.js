7956701deb1edb72002d8d4678ac9bb380c4eede
Fabio-Alfredo <00210121@uca.edu.sv>
2025-06-03 22:07:45 -0600

    :bug: bug: solution bug in mapping files with arrays a coordinates

--- src/utils/objectFlatter.util.js
+++ src/utils/objectFlatter.util.js
@@ -1,11 +1,27 @@
 export const flattenObject = (obj, parentKey = '', result = {}) => {
-    for (const [key, value] of Object.entries(obj)) {
-        const newKey = parentKey ? `${parentKey}.${key}` : key;
-        if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
-            flattenObject(value, newKey, result);
-        } else {
-            result[newKey] = value.toString ? value.toString() : value;
-        }
+  for (const [key, value] of Object.entries(obj)) {
+    const newKey = parentKey ? `${parentKey}.${key}` : key;
+
+    if (Array.isArray(value)) {
+      if (
+        key === 'coordinates' && 
+        parentKey.endsWith('geometry')
+      ) {
+        result[newKey] = value[0];
+      } else {
+        value.forEach((item, index) => {
+          if (typeof item === 'object' && item !== null) {
+            flattenObject(item, `${newKey}[${index}]`, result);
+          } else {
+            result[`${newKey}[${index}]`] = item;
+          }
+        });
+      }
+    } else if (typeof value === 'object' && value !== null) {
+      flattenObject(value, newKey, result);
+    } else {
+      result[newKey] = value;
     }
-    return result;
+  }
+  return result;
 };
